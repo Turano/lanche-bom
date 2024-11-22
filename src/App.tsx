a9cc82154e1148fdbdd887c13d7aeb5044824449
XDyao <yagopoletto@hotmail.com>
2024-11-22 19:32:54 -0300

    fix: remove debug screen from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { Cardapio } from './templates/Cardapio/index.tsx';
-import { InfosPedido } from './templates/InfosPedido/index.tsx';
 
 function App() {
   return (
     <>
       <Cardapio />
-      <InfosPedido/>
     </>
   );
 }
