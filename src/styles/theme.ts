603e8ec0d39dcefcc81f35e166ff01dcc23d8984
Antonio Luiz Carvalho Turano Filho <turano@usp.br>
2024-11-22 17:15:52 -0300

    chore(theme): update theme configuration

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,8 +1,40 @@
 export const theme = {
+  name: 'default',
   colors: {
-    mainBg: 'white',
-    secondaryBg: 'blue',
+    primary: '#EAB632',
+    secondary: '#8E1504',
+    tertiary: '#BA8379',
+    darkText: '#333333',
+    white: '#FFFFFF',
+    mediumGray: '#DDDDDD',
+    darkerGray: '#AAAAAA',
   },
-  fonts: {},
-  spacings: {},
+  font: {
+    family: {
+      default: "'Roboto', sans-serif",
+    },
+    sizes: {
+      xsmall: '1rem',
+      small: '1.6rem',
+      medium: '2.4rem',
+      large: '3.2rem',
+      huge: '5.6rem',
+    },
+  },
+  media: {
+    lteMedium: '(max-width: 768px)',
+    lteSmall: '(max-width: 360px)',
+  },
+  spacings: {
+    xsmall: '0.8rem',
+    small: '1.6rem',
+    medium: '2.4rem',
+    large: '3.2rem',
+    huge: '5.6rem',
+  },
+  sizes: {
+    max: '96rem',
+    content: '80rem',
+    radius: '0.4rem',
+  },
 };
