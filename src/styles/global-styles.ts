012ac1fb3b6b3488bc0a0102b93730b064a61358
Antonio Luiz Carvalho Turano Filho <turano@usp.br>
2024-11-22 17:16:04 -0300

    style(global-styles): update global styles

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -4,9 +4,23 @@ export const GlobalStyles = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
-  }
-  body {
-    background: ${({ theme }) => theme.colors.mainBg};
+    box-sizing: border-box;
   }
 
+  html {
+    font-size: 62.5%;
+    scroll-behavior: smooth;
+  }
+
+  body {
+    font-size: 1.6rem;
+    font-family: ${({ theme }) => theme.font.family.default};
+    color: ${({ theme }) => theme.colors.darkText};
+    background: ${({ theme }) => theme.colors.white};
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-family: ${({ theme }) => theme.font.family.default};
+    color: ${({ theme }) => theme.colors.primary};
+  }
 `;
